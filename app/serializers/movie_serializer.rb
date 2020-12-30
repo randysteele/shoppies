405922e30984eb9e7ae379eb033e8f71c8bf3cbe
steelerx155 <steelerx155@gmail.com>
2020-12-29 20:26:07 -0500

    removed unnecessary files

--- app/serializers/movie_serializer.rb
+++
@@ -1,3 +0,0 @@
-class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :Title, :Released, :Awards
-end
--- app/services/OMDBService.rb
+++
@@ -1,6 +0,0 @@
-class OMDBService
-    client = Omdb::Api::Client.new(api_key: [bc20ec47])
-
-    
-
-end
